0c7664bb08334362d9d967a72a2fb053e2fcaff6
Daniel Mrkoci <daniel@wiklunds.nu>
2022-01-13 17:25:21 +0100

    made the form print out userList, still working on email validation for registered emails

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-let userList = [
-  {
-    id: '1',
-    fName: 'Daniel',
-    lName: 'Mrkoci',
-    email: 'daniel@wiklunds.nu'
-  }
-]
+let userList = []
 
 const regForm = document.querySelector('#regForm');
 const fName = document.querySelector('#firstName');
 const lName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
-const TAC = document.querySelector('#tac');
+const tac = document.querySelector('#tac');
 const output = document.querySelector('#userList');
 const eUser = document.querySelector('#editUser');
 const rUser = document.querySelector('#removeUser');
@@ -35,41 +28,90 @@ const listUsers = () => {
   })
 }
 
-const validateText = (id) => {
-  let input = document.querySelector(id);
+const validateText = (input) => {
 
-  if(input.value === '' || input.value.length < 2) {
-    input.classList.remove('is-valid');
-    input.classList.add('is-invalid');
-    input.focus();
+  if(input.value.trim() === '') {
+    setError(input, 'Name can\'t be empty');
     return false;
   }
-  else {
-    input.classList.remove('is-invalid');
-    input.classList.add('is-valid');
+  else if(input.value.trim().length < 2) {
+    setError(input, 'Name must be atleast 2 characters long');
+    return false;
+  }
+  else{
+    setSuccess(input);
     return true;
   }
 }
 
-const validateEmail = (emailInput) => {
+const validateEmail = (email) => {
   let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-  if(regEx.test(emailInput.value)) {
-    emailInput.classList.remove('is-invalid');
-    emailInput.classList.add('is-valid');
-    return true;
+  if(email.value.trim() === '' || !regEx.test(email.value)) {
+    setError(email, 'You need to enter a valid email address');
+    return false;
   }
   else {
-    emailInput.classList.remove('is-valid');
-    emailInput.classList.add('is-invalid');
-    emailInput.focus();
-    return false;
+    setSuccess(email);
+    return true;
   }
 }
 
-listUsers();
+const validateCheck = checkbox => {
+  if(!checkbox.checked) {
+    setError(checkbox, 'You must accept the terms');
+    return false;
+  }
+  else {
+    setSuccess(checkbox);
+    return true;
+  }
+}
 
-regForm.addEventListener('submit', (e) => {
+const setError = (input, textMessage) => {
+  const parent = input.parentElement;
+  
+  parent.classList.add('is-invalid');
+  parent.classList.remove('is-valid');
+  
+  parent.querySelector('.invalid-feedback').innerText = textMessage;
+}
+
+const setSuccess = input => {
+  const parent = input.parentElement;
+  parent.classList.add('is-valid');
+  parent.classList.remove('is-invalid');
+}
+
+const validate = input => {
+  switch(input.type) {
+    case 'text': return validateText(input)
+    case 'email': return validateEmail(input)
+    case 'checkbox': return validateCheck(input)
+    default:
+      break;
+  }
+}
+
+regForm.addEventListener('submit', e => {
   e.preventDefault();
+  // listUsers();
+  // validateText();
+  // validateEmail();
+  errors = []
 
+  for(let i = 0; i < regForm.length; i++) {
+    errors[i] = validate(regForm[i]);
+  }
+  
+  if(!errors.includes(false)) {
+    const user = {
+      id: Date.now().toString(),
+      fName: fName.value,
+      lName: lName.value,
+      email: email.value
+    }
+    userList.push(user);
+    listUsers();
+  }
 })
